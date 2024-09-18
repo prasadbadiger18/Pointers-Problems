54c474a23da53e3b34441a8f1b7bd50372d65f8d
prasadbadiger18 <prasadbadiger09@gmail.com>
2024-09-18 22:19:56 +0530

    Solved First Problem

--- Pointer Types, Pointer Arithmetic, Void Pointers/Problem1.c
+++ Pointer Types, Pointer Arithmetic, Void Pointers/Problem1.c
@@ -1,1 +1,30 @@
+//1.Write a program to demonstrate different pointer types (int, float, double, char).
 
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    float b=1.23;
+    double c=0.736478;
+    char d='c';
+
+    int *intPtr;
+    intPtr=&a;
+    printf("Value stored at a : %d\n",*intPtr);
+    printf("Address of a : %d\n",intPtr);
+
+    float *floatPtr;
+    floatPtr=&b;
+    printf("Value stored at b : %f\n",*floatPtr);
+    printf("Address of b : %d\n",floatPtr);
+
+    double *doublePtr;
+    doublePtr=&c;
+    printf("Value stored at c : %f\n",*doublePtr);
+    printf("Address of c : %d\n",doublePtr);
+
+    char *charPtr;
+    charPtr=&d;
+    printf("character stored at d : %c\n",*charPtr);
+    printf("Address of d : %d\n",charPtr);
+}
